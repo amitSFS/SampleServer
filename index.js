ecac44049666f285d113dfa8ed574c6e493b9cc7
amitSFS <101413824+amitSFS@users.noreply.github.com>
2022-05-17 18:02:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,15 +16,12 @@ sslserver.listen(443, () => console.log('Secure server on port 443---'))
 app.get('/', (req,res) => {
    const credFile = fs.readFileSync("/home/ubuntu/SampleServer" + '/profile.xml', { encoding: 'utf8' })
 
-   console.log('FILE', credFile)
-   //  let data = `<?xml version="1.0" encoding="UTF-8"?>`;
-   //  data += ``;
-  res.header("Content-Type", "application/xml");
-  res.status(200).send(credFile);
+   console.log('FILE', credFile, __dirname);
+   res.header("Content-Type", "application/xml");
+   res.status(200).send(credFile);
   
 });
 
 
 // const port = process.env.PORT || 80;
-
 // app.listen(port, () => console.log(`Server is running on port ${port}...`))
